ce3eefaa92059016ed0dc8d6ab85994b7436ea77
shaludr <hemanshic@gmail.com>
2015-07-12 13:45:14 -0700

    final commits to project

--- plot3.R
+++ plot3.R
@@ -4,14 +4,19 @@ source("graph_coursera.R")
 data <- GetData()
 if (is.data.frame(data))
 { #note: png() needs to be opened before plotting, else you get a blank screen
+  #Graph plot 3
   png(filename= "plot3.png", width=480, height=480, bg = "white")
   plot(data$Time, 
        data$Sub_metering_1,
        type = "l", xlab = "", 
        ylab = "Energy sub metering")
+  
   lines(data$Time, data$Sub_metering_2, col="red")
+  
   lines(data$Time, data$Sub_metering_3, col="blue")
+  
   legend("topright", col=c("black","red","blue"),legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd= 2)
+  
   dev.off() #close this everytime so you don't have multiple devices open
   print("Plot 3 successfully graphed")
 }else 
